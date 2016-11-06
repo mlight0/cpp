cb4e86cfb1802fd7cd7948737505ce18d0d7f255
Michael Light <michael.light1@gmail.com>
2016-11-05 22:41:46 -0400

    troubleshooting

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
-echo "test 1";
+echo "test 1<br/>";
 
 $conn = new mysqli($server, $username, $password, $db);
 
