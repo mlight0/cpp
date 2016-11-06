df28525e97d7e15452174c5a732acb3db540509c
Michael Light <michael.light1@gmail.com>
2016-11-05 22:27:22 -0400

    Added MySQL connection, query and output.

--- index.php
+++ index.php
@@ -4,3 +4,38 @@ print "Hello ";
 World! <br/> 
 
 Welcome to Career Path Plus!
+
+
+<br/><br/>
+
+<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+
+$sql = "SELECT user_id, user_firs_tname, user_las_tname FROM tbl_users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "user_id: " . $row["user_id"]. " - Name: " . $row["user_first_name"]. " " . $row["user_last_name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+
+?>
